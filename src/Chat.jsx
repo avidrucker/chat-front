f21fb39d5b1cb0df1012b9a220ca2331ea5bdde2
avidrucker <avidrucker@gmail.com>
2024-07-19 14:33:48 -0400

    fix rm unused code, setup local to remote test

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
+const socket = io('http://www.adrucker.com:3000');
 
 const Chat = () => {
   const [message, setMessage] = useState('');
